eb853c8fd7b61a43f4f47d9aa2c6c2847ff243a0
K9Project <v.kh1992@mail.ru>
2024-07-04 23:12:06 +0400

    bool isOdd Added

--- PracticWorkEvenOdd.cpp
+++ PracticWorkEvenOdd.cpp
@@ -1,17 +1,10 @@
-
 #include <iostream>
 
 int main()
 {
-		int n = 0; 
-	std::cout << "Enter the number: ";
-	std::cin >> n; 
-	for (int i = 1; i < n; ++i) 
-	{
-		if (i % 2 == 0) 
-		{
-			std::cout << i << std :: endl; 
-		}
-	}
-	return 0; 
+		int n = 20; 
+		bool isOdd = true;	
+	for (int i = isOdd; i < n; i+=2)
+		std::cout << i << std :: endl; 	 
+	
 }
