5ce1fa84491ec6fb5ca7e33a7500c40c0ae344c4
K9Project <v.kh1992@mail.ru>
2024-07-07 19:00:46 +0400

    FindOdd Added

--- PracticWorkEvenOdd.cpp
+++ PracticWorkEvenOdd.cpp
@@ -2,9 +2,17 @@
 
 int main()
 {
-		int n = 20; 
-		bool isOdd = true;	
+	    int n = 13;
+
+	void FindOddNumbers(int Limit, bool isOdd);
+   {	
+		bool isOdd = true;
 	for (int i = isOdd; i < n; i+=2)
-		std::cout << i << std :: endl; 	 
-	
+		std::cout << i << std :: endl; 
+   }
+   
 }
+
+
+
+
